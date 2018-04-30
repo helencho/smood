b654a19272962848c54e0479e609ae6648ec18ee
helencho <helencho@ac.c4q.nyc>
2018-04-29 20:15:46 -0400

    set up express

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const session = require('express-session')
+const passport = require('passport')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require('./routes/index')
+// var usersRouter = require('./routes/users');
 
-var app = express();
+var app = express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(
+  session({
+    secret: `breatheofthewild`,
+    resave: false,
+    saveUninitialized: true
+  })
+)
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(express.static(path.join(__dirname, 'client/build')))
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', indexRouter)
+// app.use('/users', usersRouter);
+
+
+// Ensures frontend routes will lead to the right pages
+app.get('*', (req, res) => {
+  res.sendfile(__dirname + '/client/build/index.html')
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
