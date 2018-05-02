8f071fef2bb10168ec43a725b14ba65e63e008b6
helencho <helencho@ac.c4q.nyc>
2018-05-02 15:00:17 -0400

    divide up the routes in corresponding files

--- routes/entries.js
+++ routes/entries.js
@@ -7,37 +7,8 @@ const router = express.Router()
 const { loginRequired } = require('../auth/helpers')
 const passport = require('../auth/local')
 
-// GET
-router.get('/moods', loginRequired, dbMoods.getAllMoods)
-router.get('/moods/:moodId')
-router.get('/activities')
-router.get('/activities/:activityId')
-router.get('/entries')
-
-// POST
-router.post('/users/new')
-router.post('/users/login')
-router.post('/users/logout')
-router.post('/moods/new')
-router.post('/activities/new')
-router.post('/entries/new')
-
-// PATCH
-router.patch('/users/edit')
-router.patch('/moods/:moodId')
-router.patch('/activities/:activityId')
-
-// DELETE
-router.delete('/users/delete')
-router.delete('/moods/:moodId')
-router.delete('/activities/:activityId')
-router.delete('/entries/:entryId')
-
-
-
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
+router.get('/')
+router.post('/new')
+router.delete('/:entryId')
 
 module.exports = router;
--- routes/moods.js
+++ routes/moods.js
@@ -7,37 +7,11 @@ const router = express.Router()
 const { loginRequired } = require('../auth/helpers')
 const passport = require('../auth/local')
 
-// GET
-router.get('/moods', loginRequired, dbMoods.getAllMoods)
-router.get('/moods/:moodId')
-router.get('/activities')
-router.get('/activities/:activityId')
-router.get('/entries')
+router.get('/', loginRequired, dbMoods.getAllMoods)
+router.get('/:moodId')
+router.post('/new')
+router.patch('/:moodId')
+router.delete('/:moodId')
 
-// POST
-router.post('/users/new')
-router.post('/users/login')
-router.post('/users/logout')
-router.post('/moods/new')
-router.post('/activities/new')
-router.post('/entries/new')
-
-// PATCH
-router.patch('/users/edit')
-router.patch('/moods/:moodId')
-router.patch('/activities/:activityId')
-
-// DELETE
-router.delete('/users/delete')
-router.delete('/moods/:moodId')
-router.delete('/activities/:activityId')
-router.delete('/entries/:entryId')
-
-
-
-/* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
 
 module.exports = router;
