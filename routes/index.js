67bb47a526b7963bb85aec510661c6f537cf68e8
helencho <helencho@ac.c4q.nyc>
2018-05-02 14:18:23 -0400

    import packages for setting up db routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
-var express = require('express');
-var router = express.Router();
+const dbActivities = require('../db/queries/activities')
+const dbEntries = require('../db/entries')
+const dbMoods = require('../db/moods')
+const dbUsers = require('../db/users')
+const express = require('express')
+const router = express.Router()
+const { loginRequired } = require('../auth/helpers')
+const passport = require('../auth/local')
+
+router.get('/getAllMoods', loginRequired, dbMoods.getAllMoods);
+
+
+
+
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// router.get('/', function(req, res, next) {
+//   res.send('respond with a resource');
+// });
 
 module.exports = router;
