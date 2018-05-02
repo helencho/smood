61f0b552f9379532d7f2b4218a6c0cc5f6bad1d9
helencho <helencho@ac.c4q.nyc>
2018-05-02 12:58:00 -0400

    moods queries

--- db/queries/moods.js
+++ db/queries/moods.js
@@ -4,36 +4,99 @@ const db = require('../index')
 // /moods
 const getAllMoods = (req, res, next) => {
     db
-        .any('***', {
+        .any('SELECT * FROM moods WHERE user_id=0 OR user_id=${user_id};', {
             user_id: req.user.id
         })
         .then(data => {
             res.status(200).json({
-                status: 'success',
+                status: 'Success',
                 data: data,
                 message: 'Fetched all moods'
-            });
+            })
         })
         .catch(err => {
-            res.status(500).send(`Error fetching all moods: ${err}`);
-        });
+            res.status(500).send(`Error fetching all moods: ${err}`)
+        })
 }
 
 // GET 
 // /moods/:moodId
-const getSingleMood = (req, res, next) => { }
+const getSingleMood = (req, res, next) => {
+    db
+        .one('SELECT * FROM moods WHERE mood_id=${mood_id};', {
+            mood_id: req.params.moodId
+        })
+        .then(data => {
+            res.status(200).json({
+                status: 'Success',
+                data: data,
+                message: 'Fetched single mood'
+            })
+        })
+        .catch(err => {
+            res.status(500).send(`Error fetching single mood: ${err}`)
+        })
+}
 
 // POST 
 // /moods/new
-const createMood = (req, res, next) => { }
+const createMood = (req, res, next) => {
+    db
+        .one('INSERT INTO moods (user_id, mood_name, mood_img) VALUES (${user_id}, ${name}, ${url}) RETURNING mood_id;', {
+            user_id: req.user.id,
+            name: req.body.name,
+            url: req.body.url
+        })
+        .then(data => {
+            res.status(200).json({
+                status: 'Success',
+                data: data,
+                message: 'Created single mood'
+            })
+        })
+        .catch(err => {
+            res.status(500).send(`Error creating single mood: ${err}`)
+        })
+}
 
 // PATCH 
 // /moods/:moodId
-const updateMood = (req, res, next) => { }
+const updateMood = (req, res, next) => {
+    db
+        .none('UPDATE moods SET mood_name=${name} WHERE user_id=${user_id} AND mood_id=${mood_id};', {
+            name: req.body.name,
+            user_id: req.user.id,
+            mood_id: req.params.moodId,
+        })
+        .then(() => {
+            res.status(200).json({
+                status: 'Success',
+                message: 'Updated single mood'
+            })
+        })
+        .catch(err => {
+            res.status(500).send(`Error updating single mood: ${err}`)
+        })
+}
 
 // DELETE 
 // /moods/:moodId
-const deleteMood = (req, res, next) => { }
+const deleteMood = (req, res, next) => {
+    db
+        .none('DELETE FROM moods WHERE user_id=${user_id} AND mood_id=${mood_id};', {
+            user_id: req.user.id,
+            mood_id: req.params.moodId
+        })
+        .then(() => {
+            res.status(200).json({
+                status: 'Success',
+                message: 'Deleted single mood'
+            })
+        })
+        .catch(err => {
+            res.status(500).send(`Error deleting single mood: ${err}`)
+        })
+}
 
 
 module.exports = {
