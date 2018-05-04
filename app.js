9f49869db847fd60e3a1c2ed042776cb30c78302
helencho <helencho@ac.c4q.nyc>
2018-05-04 10:46:14 -0400

    update path

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use(
   session({
@@ -35,7 +35,7 @@ app.use(
 )
 app.use(passport.initialize())
 app.use(passport.session())
-// app.use(express.static(path.join(__dirname, 'client/build')))
+app.use(express.static(path.join(__dirname, 'client/build')))
 
 app.use('/', index)
 app.use('/users', users)
