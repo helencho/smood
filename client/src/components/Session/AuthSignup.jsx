bf0b81491d6f6a1dbc27ce9312173b66c87a5b92
helencho <helencho@ac.c4q.nyc>
2018-05-06 19:41:15 -0400

    basic signup form structure

--- client/src/components/Session/AuthSignup.jsx
+++ client/src/components/Session/AuthSignup.jsx
@@ -1,10 +1,38 @@
 import React, { Component } from 'react'
 
 class AuthSignup extends Component {
+    constructor() {
+        super()
+        this.state = {
+            name: '',
+            email: '',
+            password: ''
+        }
+    }
+
+    handleInput = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log(`Submit`)
+    }
+
     render() {
+        const { name, email, password } = this.state
+        // console.log(this.state)
+
         return (
             <div>
-                <h1>Sign Up</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" placeholder="Name" name="name" value={name} onChange={this.handleInput} />
+                    <input type="email" placeholder="Email" name="email" value={email} onChange={this.handleInput} />
+                    <input type="password" placeholder="Password" name="password" value={password} onChange={this.handleInput} />
+                    <input type="submit" placeholder="Signup" />
+                </form>
             </div>
         )
     }
