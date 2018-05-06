c68cd9030617d9398be0e99efa1562e4fc771740
helencho <helencho@ac.c4q.nyc>
2018-05-06 19:42:20 -0400

    basic login form structure

--- client/src/components/Session/AuthLogin.jsx
+++ client/src/components/Session/AuthLogin.jsx
@@ -1,10 +1,36 @@
 import React, { Component } from 'react'
 
 class AuthLogin extends Component {
+    constructor() {
+        super()
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleInput = e => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log(`Submit`)
+    }
+
     render() {
+        const { email, password } = this.state
+        // console.log(this.state)
+
         return (
             <div>
-                <h1>Log In</h1>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="email" placeholder="Email" name="email" value={email} onChange={this.handleInput} />
+                    <input type="password" placeholder="Password" name="password" value={password} onChange={this.handleInput} />
+                    <input type="submit" placeholder="Login" />
+                </form>
             </div>
         )
     }
