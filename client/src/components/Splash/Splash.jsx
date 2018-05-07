e1c57a15166d923212a3695b74de367c82aa28b7
helencho <helencho@ac.c4q.nyc>
2018-05-06 20:13:34 -0400

    basic splash structure

--- client/src/components/Splash/Splash.jsx
+++ client/src/components/Splash/Splash.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Splash extends Component {
     render() {
         return (
             <div>
-                <h1>Splash Page</h1>
+                <h1>mood</h1>
+                <p>How are you today?</p>
+                <Link to="/signup"><button>Create an account</button></Link>
+                <Link to="/login">Login</Link>
             </div>
         )
     }
