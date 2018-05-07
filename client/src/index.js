6e5fa51960da731168f4131658d4bc0ceb86eb3b
helencho <helencho@ac.c4q.nyc>
2018-05-07 13:17:22 -0400

    pass in store in root

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/root_reducer';
 import './stylesheets/index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
+const store = createStore(rootReducer);
+
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'));
 registerServiceWorker();
