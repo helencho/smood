12928ec0bc3e983c78d5d96e71427bf97f60bfe6
helencho <helencho@ac.c4q.nyc>
2018-05-07 16:09:45 -0400

    render nav based on logged in user

--- client/src/components/Navigation/HomeNav.jsx
+++ client/src/components/Navigation/HomeNav.jsx
@@ -1,17 +1,27 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
 class HomeNav extends Component {
     render() {
         return (
-            <div>
-                <Link to="/home">mood</Link>{" "}
-                <Link to="/dashboard">Dashboard</Link>{" "}
-                <Link to="/custom">Customize</Link>{" "}
-                <Link to="/profile">Profile</Link>{" "}
-            </div>
+            this.props.currentUser ?
+                <div>
+                    <Link to="/home">mood</Link>{" "}
+                    <Link to="/dashboard">Dashboard</Link>{" "}
+                    <Link to="/custom">Customize</Link>{" "}
+                    <Link to="/profile">Profile</Link>{" "}
+                </div>
+                :
+                null
         )
     }
 }
 
-export default HomeNav
+const mapStateToProps = (state) => {
+    return {
+        currentUser: state.session.currentUser
+    }
+}
+
+export default connect(mapStateToProps, null)(HomeNav)
