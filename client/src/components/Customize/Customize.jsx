16871efb2c579143ea66d338054c5d481e1ba69d
helencho <helencho@ac.c4q.nyc>
2018-05-06 20:37:42 -0400

    add links to customize page

--- client/src/components/Customize/Customize.jsx
+++ client/src/components/Customize/Customize.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class Customize extends Component {
     render() {
         return (
             <div>
                 <h1>Customize Things</h1>
+                <Link to="/custom/mood">Moods</Link>
+                <Link to="/custom/activity">Activities</Link>
             </div>
         )
     }
