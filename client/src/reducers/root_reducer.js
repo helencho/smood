d6569ab6fb7262b8ab59d1d4b0a8d8aa28716475
helencho <helencho@ac.c4q.nyc>
2018-05-06 22:01:12 -0400

    create reducer files

---
+++ client/src/reducers/root_reducer.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux'
+// Import reducers 
+
+const rootReducer = combineReducers(
+    {
+        // This is where I use imported reducers and combine them all here 
+        // session: sessionReducer
+    }
+)
+
+export default rootReducer
