7f9232c769119831c3425f40799324d31f4f521f
helencho <helencho@ac.c4q.nyc>
2018-05-08 15:15:03 -0400

    setup mood actions

--- client/src/actions/mood_actions.js
+++ client/src/actions/mood_actions.js
@@ -1,6 +1,88 @@
-// Mood actions 
+import axios from 'axios'
+export const RECEIVE_MOODS = 'RECEIVE_MOODS'
+export const RECEIVE_ERROR = 'RECEIVE_ERROR'
+
+const receiveMoods = (moods) => ({
+    type: RECEIVE_MOODS,
+    moods
+})
+
+const receiveError = (error) => ({
+    type: RECEIVE_ERROR,
+    error
+})
+
 // Get all moods 
-// Select single mood 
-// Create new mood
+export const getMoods = () => (dispatch) => {
+    axios
+        .get(`/moods`)
+        .then((data) => {
+            console.log(data)
+            // dispatch(receiveMoods(moods))
+        })
+        .catch((err) => {
+            console.log(err)
+            // dispatch(receiveError(error))
+        })
+}
+
+// Get single mood 
+export const getMood = (id) => (dispatch) => {
+    axios
+        .get(`/moods/${id}`)
+        .then((data) => {
+            console.log(data)
+            // dispatch(receiveMoods(mood))
+        })
+        .catch((err) => {
+            console.log(err)
+            // dispatch(receiveError(error))
+        })
+}
+
+// Create mood 
+export const newMood = (mood) => (dispatch) => {
+    axios
+        .post(`/moods/new`, {
+            name: mood.name,
+            url: mood.url
+        })
+        .then((data) => {
+            console.log(data)
+            // dispatch(receiveMoods(mood))
+        })
+        .catch((err) => {
+            console.log(err)
+            // dispatch(receiveError(error))
+        })
+}
+
 // Edit mood 
+export const editMood = (mood) => (dispatch) => {
+    axios
+        .patch(`/moods/${mood.id}`, {
+            name: mood.name
+        })
+        .then((data) => {
+            console.log(data)
+            // dispatch(receiveMoods(mood))
+        })
+        .catch((err) => {
+            console.log(err)
+            // dispatch(receiveError(error))
+        })
+}
+
 // Delete mood 
+export const deleteMood = (id) => (dispatch) => {
+    axios
+        .delete(`/moods/${id}`)
+        .then((data) => {
+            console.log(data)
+            // dispatch(receiveMoods(mood))
+        })
+        .catch((err) => {
+            console.log(err)
+            // dispatch(receiveError(error))
+        })
+}
--- client/src/reducers/mood_reducer.js
+++ client/src/reducers/mood_reducer.js
@@ -1,9 +1,23 @@
 // Import mood actions
+import { RECEIVE_MOODS, RECEIVE_ERROR } from '../actions/mood_actions'
 
-const initialState = {}
+const initialState = {
+    moods: [],
+    error: null
+}
 
 const moodReducer = (state = initialState, action) => {
-    return state 
+    let newState
+    switch (action.type) {
+        case RECEIVE_MOODS:
+            newState = { ...state, moods: action.moods }
+            return newState
+        case RECEIVE_ERROR:
+            newState = { ...state, error: action.error }
+            return newState
+        default:
+            return state
+    }
 }
 
 export default moodReducer 
