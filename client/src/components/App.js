93277bbec1bbab0292446cfc829ec086d56093f8
helencho <helencho@ac.c4q.nyc>
2018-05-07 20:46:24 -0400

    delete unused functions

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Switch, Route, withRouter } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 import HomeNav from './Navigation/HomeNav'
 import Splash from './Splash/Splash'
 import AuthLogin from './Session/AuthLogin'
