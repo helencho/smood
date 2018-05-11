0a4c76f207613346a5991c32b5c6748c5bd685f3
helencho <helencho@ac.c4q.nyc>
2018-05-11 11:52:50 -0400

    setup activity actions and reducers

--- client/src/actions/activity_actions.js
+++ client/src/actions/activity_actions.js
@@ -1,6 +1,39 @@
 // Activity actions
-// Get all activities
-// Select single activity
-// Create new activity
+
+import axios from 'axios'
+export const RECEIVE_ACTIVITIES = 'RECEIVE_ACTIVITIES'
+export const RECEIVE_ERROR = 'RECEIVE_ERROR'
+
+const receiveActivities = (activities) => ({
+    type: RECEIVE_ACTIVITIES,
+    activities
+})
+
+const receiveError = (error) => ({
+    type: RECEIVE_ERROR,
+    error
+})
+
+// Get all activities 
+export const getActivities = () => (dispatch) => {
+    axios
+        .get('/activities')
+        .then((data) => {
+            const activities = data.data.data
+            console.log(activities)
+            // dispatch(receiveActivities(activities))
+        })
+        .catch((err) => {
+            console.log(err)
+            const error = `Error getting all activities`
+            // dispatch(receiveError(error))
+        })
+}
+
+// Get single activity
+
+// Create activity
+
 // Edit activity
+
 // Delete activity
--- client/src/reducers/activity_reducer.js
+++ client/src/reducers/activity_reducer.js
@@ -1,9 +1,23 @@
 // Import activity actions
+import { RECEIVE_ACTIVITIES, RECEIVE_ERROR } from '../actions/activity_actions'
 
-const initialState = {}
+const initialState = {
+    activities: [],
+    error: null
+}
 
 const activityReducer = (state = initialState, action) => {
-    return state 
+    let newState
+    switch (action.type) {
+        case RECEIVE_ACTIVITIES:
+            newState = { ...state, activities: action.activities }
+            return newState
+        case RECEIVE_ERROR:
+            newState = { ...state, error: action.error }
+            return newState
+        default:
+            return state
+    }
 }
 
 export default activityReducer 
