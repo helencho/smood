dfa82bc5e5abfd4a184017ab2405f3de8b08ae07
helencho <helencho@ac.c4q.nyc>
2018-05-11 15:29:24 -0400

    ideas for mood/activity visualization

--- client/src/components/Dashboard/Dashboard.jsx
+++ client/src/components/Dashboard/Dashboard.jsx
@@ -8,9 +8,19 @@ class Dashboard extends Component {
         return (
             <div>
                 <h1>Dashboard</h1>
-                <Legend />
-                <Monthly />
-                <Yearly />
+
+                <h3>Your Moods by Year (by year, based on year clicked)</h3>
+                <p><a href="https://jsfiddle.net/alidingling/6ebcxbx4/" target="_blank">Simple radar chart</a></p>
+
+                <h3>Mood in Activities (every activity logged per mood, based on mood clicked)</h3>
+                <p><a href="https://jsfiddle.net/alidingling/9km41z5z/" target="_blank">Simple Radial Bar Chart</a></p>
+                
+                <h3>A Year in Moods (most popular mood per month for 12 months)</h3>
+                <p><a href="https://jsfiddle.net/alidingling/30763kr7/" target="_blank">Simple Bar Chart</a></p>
+
+                <h3>Activities by popularity (top 7 activities by year, based on year clicked)</h3>
+                <p><a href="https://jsfiddle.net/alidingling/30763kr7/" target="_blank">Simple Bar Chart</a></p>
+                
             </div>
         )
     }
