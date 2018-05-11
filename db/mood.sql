9c6110ff65d3cea4eb4ee39a0ccf63dabd893de5
helencho <helencho@ac.c4q.nyc>
2018-05-11 11:39:34 -0400

    reseed database with activity image urls

--- db/mood.sql
+++ db/mood.sql
@@ -68,14 +68,37 @@ VALUES
 
 INSERT INTO activities (user_id, activity_name, activity_img)
 VALUES
-    (0, 'running', 'running url'),
-    (0, 'book', 'book url'),
-    (0, 'pizza', 'pizza url'),
-    (0, 'cake', 'cake url'),
-    (0, 'eggplant', 'eggplant url'),
-    (0, 'hiking', 'hiking url'),
-    (0, 'game console', 'game console url'),
-    (0, 'rejection', 'rejection url')
+    (0, 'yoga', 'https://i.imgur.com/tXkjlKM.png'),
+    (0, 'weightlifting', 'https://i.imgur.com/GE3IUY4.png'),
+    (0, 'gaming', 'https://i.imgur.com/CzMkvWn.png'),
+    (0, 'hiking', 'https://i.imgur.com/az5kmZ0.png'),
+    (0, 'snowboarding', 'https://i.imgur.com/M35QeZc.png'),
+    (0, 'shopping', 'https://i.imgur.com/Q9wKq8A.png'),
+    (0, 'running', 'https://i.imgur.com/9fFX2ZC.png'),
+    (0, 'reading', 'https://i.imgur.com/tE363OX.png'),
+    (0, 'pottery', 'https://i.imgur.com/BbLLsSC.png'),
+    (0, 'playing the piano', 'https://i.imgur.com/W4ZQGQD.png'),
+    (0, 'photography', 'https://i.imgur.com/NrBfw0z.png'),
+    (0, 'parachuting', 'https://i.imgur.com/CIqnkAq.png'),
+    (0, 'origami', 'https://i.imgur.com/KrO2IQ9.png'),
+    (0, 'riding', 'https://i.imgur.com/pNfSmIg.png'),
+    (0, 'solving a puzzle', 'https://i.imgur.com/lllseqw.png'),
+    (0, 'hunting', 'https://i.imgur.com/E6CeDWv.png'),
+    (0, 'horseback riding', 'https://i.imgur.com/xYm6czJ.png'),
+    (0, 'playing the guitar', 'https://i.imgur.com/I1E6Qf9.png'),
+    (0, 'fishing', 'https://i.imgur.com/BZPWHbV.png'),
+    (0, 'exericsing', 'https://i.imgur.com/8wpoRXF.png'),
+    (0, 'painting', 'https://i.imgur.com/d69GcNx.png'),
+    (0, 'fixing', 'https://i.imgur.com/qgvAAqR.png'),
+    (0, 'scuba diving', 'https://i.imgur.com/Beat9QJ.png'),
+    (0, 'bicycling', 'https://i.imgur.com/SOK2kl1.png'),
+    (0, 'cooking breakfast', 'https://i.imgur.com/4RJEsjj.png'),
+    (0, 'climbing', 'https://i.imgur.com/nyI7zzl.png'),
+    (0, 'camping', 'https://i.imgur.com/nz57iVE.png'),
+    (0, 'board games', 'https://i.imgur.com/mLsEjUr.png'),
+    (0, 'baseball', 'https://i.imgur.com/9hyr5Bg.png'),
+    (0, 'baking', 'https://i.imgur.com/Ja04rFE.png'),
+    (0, 'archery', 'https://i.imgur.com/ZQ35npG.png')
 ;
 
 INSERT INTO entries (user_id, mood_id, activity_id, entry_date, note)
