c349b05e3cb9958230e4c61320c385f0d208c692
helencho <helencho@ac.c4q.nyc>
2018-05-17 14:10:26 -0400

    navigation bar to the left

--- client/src/components/Navigation/HomeNav.jsx
+++ client/src/components/Navigation/HomeNav.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getUser } from '../../actions/session_actions'
+import '../../stylesheets/navigation.css'
 
 class HomeNav extends Component {
     componentDidMount() {
@@ -11,11 +12,11 @@ class HomeNav extends Component {
     render() {
         return (
             this.props.currentUser ?
-                <div>
-                    <Link to="/"><i className="fas fa-book"></i></Link>
-                    <Link to="/dashboard"><i className="fas fa-tachometer-alt"></i></Link>
-                    <Link to="/custom"><i className="fas fa-cog"></i></Link>
-                    <Link to="/profile"><i className="far fa-user"></i></Link>
+                <div className="navigation">
+                    <Link to="/"><i className="fas fa-book fa-fw fa-3x"></i></Link>
+                    <Link to="/dashboard"><i className="fas fa-tachometer-alt fa-fw fa-3x"></i></Link>
+                    <Link to="/custom"><i className="fas fa-cog fa-fw fa-3x"></i></Link>
+                    <Link to="/profile"><i className="far fa-user fa-fw fa-3x"></i></Link>
                 </div>
                 :
                 null
