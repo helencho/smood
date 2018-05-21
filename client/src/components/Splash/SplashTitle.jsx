795c159971d4dd38e9bf170583c48b8f6372d7ee
helencho <helencho@ac.c4q.nyc>
2018-05-21 18:53:05 -0400

    add home link to mood 'logo'

--- client/src/components/Splash/SplashTitle.jsx
+++ client/src/components/Splash/SplashTitle.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class SplashTitle extends Component {
     render() {
         return (
             <div className="splash-title">
-                <h1>mood</h1>
+                <Link to="/"><h1>mood</h1></Link>
                 <p>Feeling some type of way?</p>
             </div>
         )
