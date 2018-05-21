18f9f8f6f9263463707a67b486acc8a7b48b8e81
helencho <helencho@ac.c4q.nyc>
2018-05-21 15:48:23 -0400

    slide out side menu

--- client/src/components/Navigation/HomeNav.jsx
+++ client/src/components/Navigation/HomeNav.jsx
@@ -29,13 +29,14 @@ class HomeNav extends Component {
             this.props.currentUser ?
                 <div>
                     <div className="top-nav">
-                        <button onClick={this.handleSlideout}><i className="fas fa-bars fa-fw fa-3x"></i></button>
+                        <button onClick={this.handleSlideout}><i className="fas fa-bars fa-fw fa-2x"></i></button>
+                        <h1>mood</h1>
                     </div>
                     <div className={`navigation ${navClass}`}>
-                        <Link to="/"><i className="fas fa-book fa-fw fa-3x"></i> Log</Link>
-                        <Link to="/dashboard"><i className="fas fa-tachometer-alt fa-fw fa-3x"></i> Dashboard</Link>
-                        <Link to="/custom"><i className="fas fa-cog fa-fw fa-3x"></i> Customize</Link>
-                        <Link to="/profile"><i className="far fa-user fa-fw fa-3x"></i> Profile</Link>
+                        <Link to="/"><i className="fas fa-book fa-fw fa-3x"></i><p>Log</p></Link>
+                        <Link to="/dashboard"><i className="fas fa-tachometer-alt fa-fw fa-3x"></i><p>Dashboard</p></Link>
+                        <Link to="/custom"><i className="fas fa-cog fa-fw fa-3x"></i><p>Customize</p></Link>
+                        <Link to="/profile"><i className="far fa-user fa-fw fa-3x"></i><p>Profile</p></Link>
                     </div>
                 </div>
                 :
