12bbad1a7a861af0cc49d28f24d5074d779b644e
helencho <helencho@ac.c4q.nyc>
2018-05-21 15:16:18 -0400

    strip stying to minimal, get working navbar

--- client/src/components/Navigation/HomeNav.jsx
+++ client/src/components/Navigation/HomeNav.jsx
@@ -5,18 +5,38 @@ import { getUser } from '../../actions/session_actions'
 import '../../stylesheets/navigation.css'
 
 class HomeNav extends Component {
+    constructor() {
+        super()
+        this.state = {
+            sidebar: false
+        }
+    }
+
     componentDidMount() {
         this.props.getUser()
     }
 
+    handleSlideout = () => {
+        this.setState({
+            sidebar: !this.state.sidebar
+        })
+    }
+
     render() {
+        const navClass = this.state.sidebar ? null : `navigation-hidden`
+
         return (
             this.props.currentUser ?
-                <div className="navigation">
-                    <Link to="/"><i className="fas fa-book fa-fw fa-3x"></i></Link>
-                    <Link to="/dashboard"><i className="fas fa-tachometer-alt fa-fw fa-3x"></i></Link>
-                    <Link to="/custom"><i className="fas fa-cog fa-fw fa-3x"></i></Link>
-                    <Link to="/profile"><i className="far fa-user fa-fw fa-3x"></i></Link>
+                <div>
+                    <div className="top-nav">
+                        <button onClick={this.handleSlideout}><i className="fas fa-bars fa-fw fa-3x"></i></button>
+                    </div>
+                    <div className={`navigation ${navClass}`}>
+                        <Link to="/"><i className="fas fa-book fa-fw fa-3x"></i> Log</Link>
+                        <Link to="/dashboard"><i className="fas fa-tachometer-alt fa-fw fa-3x"></i> Dashboard</Link>
+                        <Link to="/custom"><i className="fas fa-cog fa-fw fa-3x"></i> Customize</Link>
+                        <Link to="/profile"><i className="far fa-user fa-fw fa-3x"></i> Profile</Link>
+                    </div>
                 </div>
                 :
                 null
