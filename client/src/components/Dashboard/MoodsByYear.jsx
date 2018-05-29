1c03f9ad033ea64b636192e8ebe061417a8555f9
helencho <helencho@ac.c4q.nyc>
2018-05-29 12:23:28 -0400

    delete console logs

--- client/src/components/Dashboard/MoodsByYear.jsx
+++ client/src/components/Dashboard/MoodsByYear.jsx
@@ -7,43 +7,16 @@ class MoodsByYear extends Component {
     constructor() {
         super()
         this.state = {
-            year: '2019',
-            moods: {},
-            entries: []
+            year: '2019'
         }
     }
 
     componentDidMount() {
         this.props.getEntries()
-        // this.countMoods()
-    }
-
-    componentWillReceiveProps() {
-        // Set the year after user chooses year 
-    }
-
-    componentWillUpdate() {
-        // this.countMoods()
-    }
-
-    countMoods = () => {
-        // console.log('in counting moods')
-        const { year, moods } = this.state
-
-        const target = new Date(year)
-        const targetEntries = this.props.entries.filter(entry => {
-            const compare = new Date(entry.entry_date)
-            return compare <= target
-        })
-        console.log(targetEntries)
-
-        // Filter entries in the year 
-        // Count number of occurences of moods in filtered entries  
-        // Mount to state 
     }
 
     render() {
-        const { year, moods, entries } = this.state
+        const { year } = this.state
 
         // Get all the entries in the target year 
         const target = new Date(year)
@@ -51,7 +24,6 @@ class MoodsByYear extends Component {
             const compare = new Date(entry.entry_date)
             return compare <= target
         })
-        console.log(targetEntries)
 
         // Count all the moods felt in the year 
         let moodTable = {}
@@ -63,24 +35,20 @@ class MoodsByYear extends Component {
             }
         })
 
-        console.log(moodTable)
-
         // Put counted moods in a table 
         let moodData = []
         for (let mood in moodTable) {
             moodData = [...moodData, { subject: mood, A: moodTable[mood] }]
         }
 
-        console.log(moodData)
-
         return (
             <div>
-                <h3>Your Moods by Year</h3>
+                <h3>Your Moods in {year - 1}</h3>
                 <RadarChart cx={300} cy={250} outerRadius={150} width={600} height={500} data={moodData}>
                     <PolarGrid />
                     <PolarAngleAxis dataKey="subject" />
                     <PolarRadiusAxis />
-                    <Radar name="TestUser" dataKey="A" stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+                    <Radar name="TestUser" dataKey="A" stroke="#ffffff" fill="#ffffff" fillOpacity={0.6} />
                 </RadarChart>
                 <p><a href="https://jsfiddle.net/alidingling/6ebcxbx4/" target="_blank">Simple radar chart</a></p>
             </div>
