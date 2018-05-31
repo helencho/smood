521a8dfadc21afd4e77b2cfa63b2829b04e18e39
helencho <helencho@ac.c4q.nyc>
2018-05-31 11:48:24 -0400

    splash page animation

--- client/src/components/Splash/Splash.jsx
+++ client/src/components/Splash/Splash.jsx
@@ -4,11 +4,13 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getUser } from '../../actions/session_actions'
 import SplashTitle from './SplashTitle'
+import AOS from 'aos'
 import '../../stylesheets/splash.css'
 
 class Splash extends Component {
     componentDidMount() {
         this.props.getUser()
+        AOS.init()
     }
 
     render() {
@@ -18,13 +20,13 @@ class Splash extends Component {
         }
 
         return (
-            <div className="splash-container gradient-warm">
+            <div className="splash-container">
                 <SplashTitle />
                 <div className="auth-choices">
-                    <Link to="/login"><button id="button-login">Log in</button></Link>
-                    <Link to="/signup"><button id="button-register">Register</button></Link>
+                    <Link to="/login" data-aos="fade-up" data-aos-delay="100"><button id="button-login">Log in</button></Link>
+                    <Link to="/signup" data-aos="fade-up" data-aos-delay="200"><button id="button-register">Register</button></Link>
                 </div>
-                <div id="git">
+                <div id="git" data-aos="fade-up" data-aos-delay="300">
                     {/* https://github.com/asciidoctor/asciidoctor/issues/2071 */}
                     <a href="https://github.com/helencho/mood" target="_blank" rel="noopener noreferrer"><i className="fab fa-github fa-fw fa-5x"></i></a>
                 </div>
--- client/src/components/Splash/SplashTitle.jsx
+++ client/src/components/Splash/SplashTitle.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import AOS from 'aos'
 
 class SplashTitle extends Component {
     render() {
         return (
-            <div className="splash-title">
+            <div className="splash-title" data-aos="fade-up">
                 <Link to="/"><h1>mood</h1></Link>
                 <p>Feeling some type of way?</p>
             </div>
