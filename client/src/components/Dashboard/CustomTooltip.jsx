414456f180bd4d204a4692fdf2f435a1ca52b70d
helencho <helencho@ac.c4q.nyc>
2018-05-31 10:28:12 -0400

    separate out custom component

---
+++ client/src/components/Dashboard/CustomTooltip.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+class CustomTooltip extends Component {
+    render() {
+        const { active } = this.props
+        if (active) {
+            const { payload, label, data } = this.props
+
+            // Find the mood name of the current bar 
+            let moodLabel
+            data.find(entry => {
+                if (entry.name === label) {
+                    moodLabel = entry.mood
+                }
+            })
+
+            // Return the mood and count 
+            return (
+                <div className="custom-tooltip">
+                    {moodLabel ?
+                        <p className="label">{`${moodLabel} : ${payload[0].value}`}</p>
+                        : 'Nothing :('}
+                </div>
+            );
+        }
+
+        // If not active, return null 
+        return null;
+    }
+}
+
+export default CustomTooltip
