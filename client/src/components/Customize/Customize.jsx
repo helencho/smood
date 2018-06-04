4682a0614dd9ef2bc5aec83fc6aae198a8c69169
helen <helencho@ac.c4q.nyc>
2018-06-03 21:14:22 -0400

    import activity json

--- client/src/components/Customize/Customize.jsx
+++ client/src/components/Customize/Customize.jsx
@@ -1,13 +1,31 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import emotions from '../../utils/emotions.json'
+import activities from '../../utils/activities.json'
 
 class Customize extends Component {
+    constructor() {
+        super()
+        this.state = {
+            input: ''
+        }
+    }
+
+    handleInput = e => {
+        this.setState({
+            input: e.target.value
+        })
+    }
+
     render() {
+        const { input } = this.state
         console.log(emotions)
+        console.log(activities)
+
         return (
-            <div>
+            <div className="customize-container">
                 <h1>Customize Things</h1>
+                <input type='text' value={input} placeholder="Search..." onChange={this.handeInput} />
                 <Link to="/custom/mood">Moods</Link>
                 <Link to="/custom/activity">Activities</Link>
             </div>
