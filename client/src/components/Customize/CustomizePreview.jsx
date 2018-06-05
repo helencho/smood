1039a4589ef719e60d2237672feb93f837caa092
helen <helencho@ac.c4q.nyc>
2018-06-04 22:37:12 -0400

    install and import modal

--- client/src/components/Customize/CustomizePreview.jsx
+++ client/src/components/Customize/CustomizePreview.jsx
@@ -1,17 +1,48 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Modal from 'react-modal'
 
-// Presentational 
+class EditEmojiModal extends Component {
+    render() {
+        return (
+            <h1>Edit Emoji</h1>
+        )
+    }
+}
+
 class CustomizePreview extends Component {
+    constructor() {
+        super()
+        this.state = {
+            modalOpen: false
+        }
+    }
+
+    toggleModal = () => {
+        this.setState({
+            modalOpen: !this.state.modalOpen
+        })
+    }
+
     render() {
         const { customType, emojis, linkTo } = this.props
+        const { modalOpen } = this.state
 
         return (
             <div className="preview-container">
                 <h1>{customType}</h1>
                 <div className="emojis-container">
                     {emojis.map((emoji, index) => (
-                        <p key={index}>{emoji.img}</p>
+                        <div>
+                            <p onClick={this.toggleModal} key={index}>{emoji.img}</p>
+                            <Modal
+                                isOpen={modalOpen}
+                                onRequestClose={this.toggleModal}
+                                contentLabel="Edit Emoji Modal"
+                            >
+                                <EditEmojiModal />
+                            </Modal>
+                        </div>
                     ))}
                     <Link to={`/custom/${linkTo}`}><button><i className="fas fa-plus fa-fw" /></button></Link>
                 </div>
