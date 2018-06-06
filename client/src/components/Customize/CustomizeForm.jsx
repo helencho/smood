978984b08e5393f00bd58dcd763376e4f62f5fcd
helen <helencho@ac.c4q.nyc>
2018-06-05 20:40:35 -0400

    divide up into sections

--- client/src/components/Customize/CustomizeForm.jsx
+++ client/src/components/Customize/CustomizeForm.jsx
@@ -3,15 +3,17 @@ import React, { Component } from 'react'
 // Presentational 
 class CustomizeForm extends Component {
     render() {
-        const { customType, emojis, input, chosenEmoji, chosenName, handleSubmit, handleInput, handleClick } = this.props 
+        const { customType, emojis, input, chosenEmoji, chosenName, handleSubmit, handleInput, handleClick } = this.props
 
         return (
             <div className="customize-form-container">
                 <h1>Customize your {customType}</h1>
                 <form className="chosen-container" onSubmit={handleSubmit}>
                     <p>{chosenEmoji}</p>
-                    <input type="text" placeholder="Name" name="chosenName" value={chosenName} onChange={handleInput} />
-                    <input type="submit" value="Save" disabled={!chosenEmoji || !chosenName} />
+                    <div className="inputs">
+                        <input type="text" placeholder="Name" name="chosenName" value={chosenName} onChange={handleInput} />
+                        <input type="submit" value="Save" disabled={!chosenEmoji || !chosenName} />
+                    </div>
                 </form>
                 <div className="emojis-container">
                     {emojis.map((mood, index) => (
